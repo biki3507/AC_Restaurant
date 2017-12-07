1eaefa77d075ab6c1915a70935443ec0bf4aaac5
Hazel Wu <tinazx056@gmail.com>
2017-12-07 13:56:54 +0800

    Added backstage admin authentication & login message & login button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "不允許進入"
+      redirect_to root_path
+    end
+  end
+
 end
