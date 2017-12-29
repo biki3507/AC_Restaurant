8a375ddb185681399c9b8b4a2280c9e3e40adaf0
Hazel Wu <tinazx056@gmail.com>
2017-12-29 15:27:29 +0800

    Setup seed data for default categories and Admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 清空資料表
+Category.destroy_all
+
+# 設定種子資料
+category_list = [
+    { name: "中式料理" },
+    { name: "日本料理" },
+    { name: "義大利料理" },
+    { name: "墨西哥料理" },
+    { name: "素食料理" },
+    { name: "美式料理" },
+    { name: "複合式料理" } 
+]
+
+# 建立種子資料
+category_list.each do |category|
+    Category.create(name: category[:name])
+end
+# 提示訊息
+puts "Caretory 分類標籤 has been created!"
+
+# 順便建立 Admin 的帳號
+User.create(email: "root@gmail.com",
+            password: "123456",
+            role: "admin")
+puts "Default Admin created !"
